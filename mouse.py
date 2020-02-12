3c229a3e52f6a597aea63f36918bbef5463433d6
Lorne <dev@luotao.net>
2020-02-12 21:28:26 +1100

    update mouse move

--- mouse.py
+++ mouse.py
@@ -1,15 +1,31 @@
 import time
 
-from ctypes.windll.user32 import mouse_event
+import ctypes
+from pyautogui._window_win import getWindow
+import pyautogui as pag
+from wows import *
+
 
 MOUSEEVENT_MOVE = 0x0001
-
+FACTOR = 1.3
+LOOP = 10
 
 def move_mouse(x, y):
-    mouse_event(MOUSEEVENT_MOVE, x, y, 0, 0)
+    time.sleep(1)
+    x = x/LOOP/FACTOR
+    y = y/LOOP/FACTOR
 
+    for i in range(10):
+        ctypes.windll.user32.move_mouse(x, y, 0, 0)
+        time.sleep(0.01)
 
 if __name__ == '__main__':
-    for i in range(100):
-        move_mouse(1, 1)
-        time.sleep(0.01)
+    wows_window = getWindow('World of Warships')
+    wows_window.set_foreground()  # switch to wows window
+
+    # move_ship()
+    # fire_ship()
+    # move_mouse(400, 0)
+    y = -50
+    x = 500
+    move_mouse(x, y)
